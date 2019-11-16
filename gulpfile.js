2cad936055668f0ef9ff0816e358ab95f1cc1612
oksanakritskayar <sonce8881@mail.ru>
2019-11-17 01:13:39 +0700

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,35 @@ const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
-gulp.task('run', function () {
+exports.run = function () {
     browserSync.init({
         server: {
             baseDir: 'src/'
         }
     });
 
-    gulp.watch(['src/styles/*.scss', 'src/styles/**/*.scss'], gulp.series('sass'));
+    gulp.watch(['src/styles/*.scss', 'src/styles/**/*.scss'], scss_watch);
     gulp.watch('src/*.html').on('change', browserSync.reload);
+};
+
+exports.build = gulp.series(scss, function () {
+    return gulp
+        .src(['src/**/*.svg', 'src/*.html'])
+        .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('sass', function () {
-    return gulp.src(['src/styles/*.scss', 'src/styles/**/*.scss'])
+function scss() {
+    return gulp
+        .src(['src/styles/*.scss', 'src/styles/**/*.scss'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css'))
+        .pipe(gulp.dest('dist/css'));
+}
+
+function scss_watch() {
+    return gulp
+        .src(['src/styles/*.scss', 'src/styles/**/*.scss'])
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.stream());
-});
-
-gulp.task('build', function () {
-    return gulp.src(['src/styles/*.scss', 'src/styles/**/*.scss'], ['sass'])
-        .pipe(gulp.dest('dist/styles/'));
-});
+}
