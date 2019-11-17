c5d53a9fcb8a9258edc17c67527acd6332bf4c9c
oksanakritskayar <sonce8881@mail.ru>
2019-11-17 14:47:31 +0700

    Added svg-sprite to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
+const merge = require('merge-stream');
+const streamqueue = require('streamqueue');
 
 const browserSync = require('browser-sync');
+const svgSprite = require('gulp-svg-sprite');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
@@ -11,28 +14,48 @@ exports.run = function () {
         }
     });
 
-    gulp.watch(['src/styles/*.scss', 'src/styles/**/*.scss'], scss_watch);
+    gulp.watch(['src/styles/*.scss', 'src/styles/**/*.scss'], css);
+    gulp.watch('src/images/icons/*.svg', svg);
     gulp.watch('src/*.html').on('change', browserSync.reload);
 };
 
-exports.build = gulp.series(scss, function () {
-    return gulp
-        .src(['src/**/*.svg', 'src/*.html'])
-        .pipe(gulp.dest('dist/'));
-});
+exports.build = function () {
+    const css_ouput = streamqueue({objectMode: true},
+        css,
+        gulp.src(['src/css/*.css', 'src/css/**/*.css']))
+        .pipe(gulp.dest('dist/styles'));
 
-function scss() {
+    const svg_ouput = streamqueue({objectMode: true},
+        svg,
+        gulp.src('src/images/*.svg'))
+        .pipe(gulp.dest('dist/images'));
+
+    return merge(
+        css_ouput,
+        svg_ouput,
+        gulp.src('src/*.html')
+            .pipe(gulp.dest('dist'))
+    );
+};
+
+function css() {
     return gulp
         .src(['src/styles/*.scss', 'src/styles/**/*.scss'])
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('src/css'))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(browserSync.stream());
 }
 
-function scss_watch() {
+function svg() {
     return gulp
-        .src(['src/styles/*.scss', 'src/styles/**/*.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('src/css'))
+        .src('src/images/icons/*.svg')
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: "../sprite.svg"
+                }
+            },
+        }))
+        .pipe(gulp.dest('src/images'))
         .pipe(browserSync.stream());
 }
