90f99ac97047ce1abd5b2bc11ef8e0c594edfd0b
oksanakritskayar <sonce8881@mail.ru>
2019-11-24 00:59:19 +0700

    Added profile page

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,6 @@ exports.run = function () {
     css();
     svg();
     html();
-    html_components();
 
     browserSync.init({
         server: {
@@ -20,9 +19,9 @@ exports.run = function () {
         }
     });
 
-    gulp.watch(['src/styles/*.scss', 'src/styles/**/*.scss'], gulp.series(css, browserSync.reload));
+    gulp.watch(['src/styles/*.scss', 'src/styles/**/*.scss'], css);
     gulp.watch('src/images/icons/*.svg', svg);
-    gulp.watch(['src/template/*.html', 'src/template/**/*.html'], gulp.parallel(html, html_components));
+    gulp.watch(['src/template/*.html', 'src/template/**/*.html'], html);
     gulp.watch('src/*.html').on('change', browserSync.reload);
 };
 
@@ -42,12 +41,7 @@ exports.build = function () {
         gulp.src('src/*.html'))
         .pipe(gulp.dest('dist'));
 
-    const html_components_ouput = streamqueue({objectMode: true},
-        html_components,
-        gulp.src('src/components/*.html'))
-        .pipe(gulp.dest('dist/components'));
-
-    return merge(css_ouput, svg_ouput, html_ouput, html_components_ouput);
+    return merge(css_ouput, svg_ouput, html_ouput);
 };
 
 function css() {
@@ -60,7 +54,7 @@ function css() {
 
 function svg() {
     return gulp
-        .src('src/images/icons/*.svg')
+        .src('src/images/icons/*.*')
         .pipe(svgSprite({
             mode: {
                 stack: {
@@ -73,17 +67,11 @@ function svg() {
 }
 
 function html() {
-    return gulp.src(['src/template/*.html'])
+    return gulp.src(['src/template/**/*', '!src/template/_*/*'])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
         .pipe(gulp.dest('src/'))
-        .pipe(browserSync.stream());
-}
-
-function html_components() {
-    return gulp.src(['src/template/components/*.html'])
-        .pipe(gulp.dest('src/components'))
         .pipe(browserSync.stream());
 }
